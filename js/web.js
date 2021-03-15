78838e336aca3dd860da2306774855fd80f4473c
Rainless1125 <ah247517m@gmail.com>
2021-03-15 18:24:33 +0800

    change bgcolor and scroll up delay time

--- js/web.js
+++ js/web.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
     $("#gotop").hide();
 
     $("#gotop").click(function(){
-		$('html,body').animate({ scrollTop: 0}, 1200);
+		$('html,body').animate({ scrollTop: 0}, 500);
     });	
     $(window).scroll(function(){
 		if ($(this).scrollTop() > 500){
